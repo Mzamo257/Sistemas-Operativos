e15ec69ee952e8c0715a32f99558b2c51ca33e7b
Mzamo <molimica@gmail.com>
2016-11-14 10:39:13 +0100

    Iniciado

---
+++ TrabajoSistemas/src/EntradaFat.java
@@ -0,0 +1,20 @@
+
+public class EntradaFat {
+
+	private	boolean disponible;
+	private	boolean fin;
+	private	int siguiente;
+		
+	public boolean getDispoible()
+	{
+		return disponible;
+	}
+	
+	public boolean getFin()
+	{
+		return fin;
+	}
+	
+	
+		
+}
